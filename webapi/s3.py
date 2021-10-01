726af2e5482fcfa98b0b5dd306637b1c5e48812d
Silas Carvalho <silasalvares@gmail.com>
2021-10-01 20:29:24 -0300

    fixing s3 auth

--- webapi/s3.py
+++ webapi/s3.py
@@ -9,7 +9,7 @@ class S3ImageHandler():
             's3', 
             region_name=os.environ.get('S3_REGION'),
             aws_access_key_id=os.environ.get('ACCESS_ID'),
-            aws_secret_access_key=os.environ.get('ACCESS_KEY')))
+            aws_secret_access_key=os.environ.get('ACCESS_KEY'))
 
     def upload_image(self, key, image):
         self.s3_client.put_object(Bucket=os.environ.get('IMAGE_BUCKET'), Key=key, Body=image)
