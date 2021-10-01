2487aaf02126c3030463c5aad515534688269b6f
Silas Carvalho <silasalvares@gmail.com>
2021-10-01 20:28:00 -0300

    fixing s3 auth

--- webapi/s3.py
+++ webapi/s3.py
@@ -5,7 +5,11 @@ from botocore.client import Config
 class S3ImageHandler():
 
     def __init__(self):
-        self.s3_client = boto3.client('s3', region_name=os.environ.get('S3_REGION'))
+        self.s3_client = boto3.client(
+            's3', 
+            region_name=os.environ.get('S3_REGION'),
+            aws_access_key_id=os.environ.get('ACCESS_ID'),
+            aws_secret_access_key=os.environ.get('ACCESS_KEY')))
 
     def upload_image(self, key, image):
         self.s3_client.put_object(Bucket=os.environ.get('IMAGE_BUCKET'), Key=key, Body=image)
